8856a732e03ae6570929e9d6f0f029d20d379dd4
MaksymMykhailets <149175463+MaksymMykhailets@users.noreply.github.com>
2024-09-12 22:28:42 +0200

    water progress barr + improvements

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -2,12 +2,10 @@ import styles from "./WaterDailyNorma.module.css";
 
 const WaterDailyNorma = () => {
   return (
-    <>
-      <div className={styles.normaWrapper}>
-        <p className={styles.normaValue}>1.5 L</p>
-        <p className={styles.normaDesc}>My daily norma</p>
-      </div>
-    </>
+    <div className={styles.normaWrapper}>
+      <p className={styles.normaValue}>1.5 L</p>
+      <p className={styles.normaDesc}>My daily norma</p>
+    </div>
   );
 };
 
