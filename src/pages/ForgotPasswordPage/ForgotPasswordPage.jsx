b3042c312ec726582d6946d54338eacd46082033
IrinaCota <iryna.kotsa91@gmail.com>
2024-09-18 12:06:04 +0100

    add extra styles

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -1,7 +1,20 @@
 import ForgotPasswordForm from "../../components/ForgotPasswordForm/ForgotPasswordForm";
+import Logo from "../../components/Logo/Logo";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
+import css from "./ForgotPassword.module.css"
 
 const ForgotPasswordPage = () => {
-  return <ForgotPasswordForm />;
+  return (
+    <div className={css.ForgotSection}>
+      <div className={css.container}>
+        <Logo />
+        <ForgotPasswordForm />
+      </div>
+      <div className={css.advantages}>
+        <AdvantagesSection />
+      </div>
+    </div>
+  );
 };
 
 export default ForgotPasswordPage;
--- src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,7 +1,20 @@
 import ResetPasswordForm from "../../components/ResetPasswordForm/ResetPasswordForm";
+import Logo from "../../components/Logo/Logo";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
+import css from "./ResetPassword.module.css"
 
 const ResetPasswordPage = () => {
-  return <ResetPasswordForm />;
+  return (
+    <div className={css.ResetSection}>
+      <div className={css.container}>
+        <Logo />
+        <ResetPasswordForm />
+      </div>
+      <div className={css.advantages}>
+        <AdvantagesSection />
+      </div>
+    </div>
+  );
 };
 
 export default ResetPasswordPage;
