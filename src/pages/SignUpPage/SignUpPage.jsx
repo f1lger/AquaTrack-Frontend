d33a986fa9a0f621fd2aee32732c6e23047ac5cb
Volodymyr <palcinskijvolodimir173@gmail.com>
2024-09-19 18:48:28 +0300

    fix advantages on signup page

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,12 +1,18 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import Logo from '../../components/Logo/Logo';
 import styles from './SignUpPage.module.css'
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 
 export default function SignUpPage() {
   return (
     <div className={styles.wrapper}>
-      <Logo/>
-      <SignUpForm />
+      <div className={styles.container}>
+        <Logo />
+        <SignUpForm />
+      </div>
+      <div className={styles.advantages}>
+        <AdvantagesSection />
+      </div>
     </div>
   );
 }
