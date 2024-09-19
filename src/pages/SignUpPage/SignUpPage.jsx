602dab2fd28da201ab3e7862d2702661efa5b1cb
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-09-19 19:33:31 +0300

    add error styles, add password hidding btn

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,12 +1,18 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
-import Logo from '../../components/Logo/Logo';
-import styles from './SignUpPage.module.css'
+import Logo from "../../components/Logo/Logo";
+import styles from "./SignUpPage.module.css";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 
 export default function SignUpPage() {
   return (
     <div className={styles.wrapper}>
-      <Logo/>
-      <SignUpForm />
+      <div className={styles.signUpSection}>
+        <Logo />
+        <SignUpForm />
+      </div>
+      <div className={styles.advSection}>
+        <AdvantagesSection />
+      </div>
     </div>
   );
 }
