ec1f235542388eca2b6732a89ee2d28f0cd3c131
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-09-21 03:38:02 +0300

    Fixed day styles

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -3,19 +3,21 @@ import { useSelector } from "react-redux";
 import { selectMonthlyRecords } from "../../redux/water/selectors";
 import { selectDailyNorma } from "../../redux/auth/selectors";
 import clsx from "clsx";
+import { useMemo } from "react";
 
 const Calendar = ({ daysInMonth }) => {
   const monthlyRecords = useSelector(selectMonthlyRecords);
   const dailyTarget = useSelector(selectDailyNorma);
 
-  const daysArray = Array.from({ length: daysInMonth }, (_, index) => {
-    const day = index + 1;
-    const record = monthlyRecords.find((rec) => rec[day]);
-    const waterConsumed = record ? record[day] : 0;
-    const percentage = (waterConsumed / dailyTarget) * 100;
-
-    return Math.min(percentage, 100).toFixed(0);
-  });
+  const daysArray = useMemo(() => {
+    return Array.from({ length: daysInMonth }, (_, index) => {
+      const day = index + 1;
+      const record = monthlyRecords.find((rec) => rec[day]);
+      const waterConsumed = record ? record[day] : 0;
+      const percentage = (waterConsumed / dailyTarget) * 100;
+      return Math.min(percentage, 100).toFixed(0);
+    });
+  }, [daysInMonth, monthlyRecords, dailyTarget]);
 
   return (
     <div>
@@ -24,6 +26,7 @@ const Calendar = ({ daysInMonth }) => {
           <li key={index + 1} className={css.calendarDay}>
             <button
               className={clsx(
+                css.baseDay,
                 percentage === "100" ? css.waterCompleted : css.day
               )}
             >
