29eb558fdfb497378729fd824d95542da40aae29
ok-qa <olechka.kr@gmail.com>
2024-09-21 16:46:01 -0700

    DailyInfo added
    Components:
    - DailyInfo
    - ChooseDate
    - AddWaterDailyBtn
    - WaterList
    - WaterItem

--- src/components/DaillyInfo/DailyInfo.jsx
+++ src/components/DaillyInfo/DailyInfo.jsx
@@ -1,17 +1,14 @@
-import ChooseDate from "../DailyInfo/ChooseDate/ChooseDate.jsx";
-import AddWaterDailyBtn from "./AddWaterDailyBtn/AddWaterDailyBtn.jsx";
 import css from "./DailyInfo.module.css";
-import WaterList from "./WaterList/WaterList";
 
 const DailyInfo = () => {
   return (
     <section className={css.section}>
       <div className={css.container}>
         <div className={css.title}>
-          <ChooseDate />
-          <AddWaterDailyBtn />
+          {/* <ChooseDate /> */}
+          {/* <AddWaterDailyBtn /> */}
         </div>
-        <WaterList />
+        {/* <WaterList /> */}
       </div>
     </section>
   );
