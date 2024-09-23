25c623bf6d2f91ca8a5152a5f4dbad9bacfce313
Volodymyr <palcinskijvolodimir173@gmail.com>
2024-09-23 12:11:37 +0300

    Update ModalBtn.jsx

--- src/components/ModalBtn/ModalBtn.jsx
+++ src/components/ModalBtn/ModalBtn.jsx
@@ -1,36 +1,36 @@
 
 
-import PropTypes from 'prop-types';
-import css from "./ModalBtn.module.css";
+// import PropTypes from 'prop-types';
+// import css from "./ModalBtn.module.css";
 
-export const ModalBtn = ({ text, onClick }) => {
-  const getButtonClass = () => {
-    if (text === "Save") return css.saveBtn;
-    if (text === "Log out") return css.logOutBtn;
-    if (text === "Cancel") return css.cancelBtn;
-    if (text === "Delete") return css.logOutBtn;
-    return "";
-  };
+// export const ModalBtn = ({ text, onClick }) => {
+//   const getButtonClass = () => {
+//     if (text === "Save") return css.saveBtn;
+//     if (text === "Log out") return css.logOutBtn;
+//     if (text === "Cancel") return css.cancelBtn;
+//     if (text === "Delete") return css.logOutBtn;
+//     return "";
+//   };
 
-  return (
-    <button className={getButtonClass()} onClick={onClick} type="submit">
-      <p
-        className={
-          text === "Save" || text === "Log out" || text === "Delete"
-            ? css.saveBtnText
-            : css.cancelBtnText
-        }
-      >
-        {text}
-      </p>
-    </button>
-  );
-};
+//   return (
+//     <button className={getButtonClass()} onClick={onClick} type="submit">
+//       <p
+//         className={
+//           text === "Save" || text === "Log out" || text === "Delete"
+//             ? css.saveBtnText
+//             : css.cancelBtnText
+//         }
+//       >
+//         {text}
+//       </p>
+//     </button>
+//   );
+// };
 
-ModalBtn.propTypes = {
-  text: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
+// ModalBtn.propTypes = {
+//   text: PropTypes.string.isRequired,
+//   onClick: PropTypes.func.isRequired,
+// };
 
 
 /*
