f2c7f6d288454d8ae0a92ef09197fe0401ccea79
Aleksander <aleksander.0705@gmail.com>
2024-09-24 20:38:05 +0300

    fix percent

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,37 +1,70 @@
 import css from "./Calendar.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectMonthlyRecords } from "../../redux/water/selectors";
+import {
+  selectDailyRecords,
+  selectMonthlyRecords,
+} from "../../redux/water/selectors";
 import { selectDailyNorma } from "../../redux/auth/selectors";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import CalendarItem from "../CalendarItem/CalendarItem";
-import { waterPerDay } from "../../redux/water/operations";
-import { setSelectedDate } from "../../redux/water/slice";
+import { waterPerDay, waterPerMonth } from "../../redux/water/operations";
 
 const Calendar = ({ daysInMonth, year, month }) => {
   const dispatch = useDispatch();
   const monthlyRecords = useSelector(selectMonthlyRecords);
   const dailyTarget = useSelector(selectDailyNorma);
+  const dailyRecords = useSelector(selectDailyRecords);
 
   const [activeIndex, setActiveIndex] = useState(null);
 
+  const today = new Date();
+  const currentDay = today.getDate();
+  const currentMonth = today.getMonth() + 1;
+  const currentYear = today.getFullYear();
+
+  const formattedMonth = `${year}-${String(month).padStart(2, "0")}`;
+
+  useEffect(() => {
+    dispatch(waterPerMonth(formattedMonth));
+  }, [dispatch, formattedMonth]);
+
   const handleButtonClick = (index) => {
     setActiveIndex(index);
-    const formattedMonth = String(month).padStart(2, "0");
-    const date = `${year}-${formattedMonth}-${index + 1}`;
-
+    const date = `${year}-${String(month).padStart(2, "0")}-${index + 1}`;
     dispatch(waterPerDay(date));
-    dispatch(setSelectedDate(date));
   };
 
   const daysArray = useMemo(() => {
     return Array.from({ length: daysInMonth }, (_, index) => {
       const day = index + 1;
+
+      if (
+        day === currentDay &&
+        month === currentMonth &&
+        year === currentYear
+      ) {
+        const totalWaterToday = dailyRecords.reduce(
+          (sum, record) => sum + record.amount,
+          0
+        );
+        const percentageToday = (totalWaterToday / dailyTarget) * 100;
+        return Math.min(percentageToday, 100).toFixed(0);
+      }
+
       const record = monthlyRecords.find((rec) => rec[day]);
       const waterConsumed = record ? record[day] : 0;
       const percentage = (waterConsumed / dailyTarget) * 100;
       return Math.min(percentage, 100).toFixed(0);
     });
-  }, [daysInMonth, monthlyRecords, dailyTarget]);
+  }, [
+    daysInMonth,
+    monthlyRecords,
+    dailyTarget,
+    dailyRecords,
+    currentDay,
+    currentMonth,
+    currentYear,
+  ]);
 
   return (
     <div>
