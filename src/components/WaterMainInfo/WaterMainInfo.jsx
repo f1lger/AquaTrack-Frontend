f5bacc69d2e39a4ee1f8635d4b5866d71e645ece
Volodymyr <palcinskijvolodimir173@gmail.com>
2024-09-25 15:11:50 +0300

    Update WaterMainInfo.jsx

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -2,6 +2,7 @@ import WaterDailyNorma from "../WaterDailyNorma/WaterDailyNorma";
 import WaterProgressBar from "../WaterProgressBar/WaterProgressBar";
 import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
 import styles from "./WaterMainInfo.module.css";
+import { Link } from "react-router-dom";
 
 
 const WaterMainInfo = ({ openWaterModal }) => {
@@ -9,7 +10,9 @@ const WaterMainInfo = ({ openWaterModal }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.thumb} />
-      <h1 className={styles.title}>AquaTrack</h1>
+      <Link to="/">
+        <h1 className={styles.title}>AquaTrack</h1>
+      </Link>
       <WaterDailyNorma />
       <WaterProgressBar />
       <AddWaterBtn openWaterModal={openWaterModal} />
